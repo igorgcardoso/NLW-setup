bf27e5e57587fd0d7cf316429c0081830df58587
Igor Cardoso <igorgcardoso7@gmail.com>
2023-01-23 09:53:59 -0300

    Aula 5

--- mobile/src/utils/generate-progress-porcentage.ts
+++ mobile/src/utils/generate-progress-porcentage.ts
@@ -1,4 +1,4 @@
 export function generateProgressPercentage(total: number, completed: number) {
   if (completed === 0 || total === 0) return 0;
-  return Math.round(completed / total) * 100;
+  return Math.round((completed / total) * 100);
 }
